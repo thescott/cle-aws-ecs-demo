f528c25d6674ddd75ab5a5f2cc20a924ece1155b
thescott <scott.mabe@me.com>
2019-05-10 08:04:03 -0400

    inital push

---
+++ src/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Cleveland AWS Meetup</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+        <style>body {margin-top: 40px; background-color: #333;}</style>
+        <link href="assets/css/bootstrap-responsive.min.css" rel="stylesheet">
+        <!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+    </head>
+<iframe src="https://giphy.com/embed/3ornk57KwDXf81rjWM" width="480" height="259" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/starwars-movie-star-wars-3ornk57KwDXf81rjWM">via GIPHY</a></p>
+    
+    <body>
+        <div class="container">
+            <div class="hero-unit">
+                <h1>Cleveland AWS Meetup</h1>
+                <h2>Version 1</h2>
+                <p>This application is now running on a container in Amazon ECS.</p>
+                <p>From here, you can commit a change to the Github repo. The CodePipeline will run and update the ECS cluster.</p>
+                <p>For example, change <pre>Version 1</pre> above to <pre>Version 2</pre>.</p>
+                <?php
+                        $myfile = fopen("/var/www/my-vol/date", "r") or die("");
+                        echo fread($myfile,filesize("/var/www/my-vol/date"));
+                        fclose($myfile);
+                ?>
+
+            </div>
+        </div>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+    </body>
+
+</html>
